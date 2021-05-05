a43606588874ceebe132add829ce917ea999e444
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-05-05 13:30:23 +0530

    aroha_react_static_website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import{Route, Switch} from "react-router-dom";
+import Homepage from './components/homepage';
+import Footer from './components/footer'; 
+import AboutLearning360 from './components/aboutlearning360';
+import NavBar from './components/navbar';
+import AppsInPlatform from './components/appsinplatform'; 
+import AboutMy360 from './components/aboutmy360';
+import SalesAnalytics from './components/salesanalytics';
+import Cashflowanalytics from './components/cashflowanalytics';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar/>
+
+      <main role="main" > 
+
+        <Switch> 
+      
+        <Route path="/aboutLearning" component={AboutLearning360} /> 
+        <Route path="/appsinlearning" component={AppsInPlatform} />
+        <Route path="/aboutMy360" component={ AboutMy360} /> 
+        <Route path="/salesAnalytics" component={SalesAnalytics} />
+       <Route path="/financialAnalytics" component={Cashflowanalytics} /> 
+       <Route path="/home" component={Homepage} />
+       <Homepage/>
+       
+
+          
+
+         
+
+        </Switch> 
+
+      </main>
+
+
+      <Footer />
+
+    </React.Fragment>
   );
 }
 
